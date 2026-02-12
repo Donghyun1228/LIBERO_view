48603bd8fc8113683fac92015e210502fcd7c7c8
Donghyun1228 <dawnme1228@naver.com>
2026-02-12 15:06:32 +0900

    feat: robot initial position perturbation

--- libero/libero/envs/robots/__init__.py
+++ libero/libero/envs/robots/__init__.py
@@ -1,5 +1,5 @@
-from .mounted_panda import MountedPanda
-from .on_the_ground_panda import OnTheGroundPanda
+from .mounted_panda import MountedPanda, MountedPanda1
+from .on_the_ground_panda import OnTheGroundPanda, OnTheGroundPanda1
 
 from robosuite.robots.single_arm import SingleArm
 from robosuite.robots import ROBOT_CLASS_MAPPING
@@ -8,5 +8,7 @@ ROBOT_CLASS_MAPPING.update(
     {
         "MountedPanda": SingleArm,
         "OnTheGroundPanda": SingleArm,
+        "MountedPanda1": SingleArm,
+        "OnTheGroundPanda1": SingleArm,
     }
 )
